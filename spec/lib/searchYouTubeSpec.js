ac4c40b6ed8612081f29936688279ff294dee320
Rikki Sasda <rikkisasda@gmail.com>
2018-10-02 17:43:19 -0500

    set up searchYoutube.js

--- spec/lib/searchYouTubeSpec.js
+++ spec/lib/searchYouTubeSpec.js
@@ -60,10 +60,10 @@ describe('searchYouTube', function() {
   });
 
   it('should accept `key`, `query`, and `max` options and send them in GET request', function() {
-    searchYouTube({ key: 'API_KEY', query: 'cats', max: 10 }, () => {});
+    searchYouTube({ key: YOUTUBE_API_KEY, query: 'cats', max: 10 }, () => {});
 
     var params = getURLSearchParams(requests[0].url);
-    expect(params.key).to.equal('API_KEY');
+    expect(params.key).to.equal(YOUTUBE_API_KEY);
     expect(params.q).to.equal('cats');
     expect(params.maxResults).to.equal('10');
   });
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,19 @@
+import YOUTUBE_API_KEY from '../config/youtube.js';
+
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      part: 'snippet',
+      q: options.query,
+      maxResults: options.max,
+      key: YOUTUBE_API_KEY,
+    },
+    type: 'GET',
+    success: function(data) {
+      callback(data);
+    },
+  });
 };
 
 export default searchYouTube;
